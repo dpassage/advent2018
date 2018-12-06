eab97eea827edd5cc45fa9cba34ac4f577f0e912
David Paschich <dpassage@balveda.com>
2018-12-06 12:56:11 -0800

    Day 1 Part 2

--- Advent2018.playground/Pages/Day 1 - Chronal Calibration.xcplaygroundpage/Contents.swift
+++ Advent2018.playground/Pages/Day 1 - Chronal Calibration.xcplaygroundpage/Contents.swift
@@ -18,4 +18,30 @@ let day1lines = day1.components(separatedBy: "\n")
 
 print(calibrate(input: day1lines))
 
+func findRepat(input: [String]) -> Int {
+    let changes = input.compactMap { Int($0) }
+
+    var soFar: Set<Int> = [0]
+
+    var currentFrequency = 0
+
+    while true {
+        for change in changes {
+            currentFrequency += change
+            if soFar.contains(currentFrequency) {
+                return currentFrequency
+            } else {
+                soFar.insert(currentFrequency)
+            }
+        }
+    }
+}
+
+print(findRepat(input: "+1, -2, +3, +1".components(separatedBy: ", ")))
+print(findRepat(input: "+1, -1".components(separatedBy: ", ")))
+print(findRepat(input: "+3, +3, +4, -2, -4".components(separatedBy: ", ")))
+print(findRepat(input: "-6, +3, +8, +5, -6".components(separatedBy: ", ")))
+print(findRepat(input: "+7, +7, -2, -7, -4".components(separatedBy: ", ")))
+
+print(findRepat(input: day1lines))
 //: [Next](@next)
