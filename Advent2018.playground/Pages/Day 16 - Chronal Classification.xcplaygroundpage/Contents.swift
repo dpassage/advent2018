fb110bd6cb001c6cb3dda7a1ed044c6f33ec22e7
David Paschich <dpassage@balveda.com>
2018-12-23 10:26:05 -0800

    training of instruction set - which doesn't narrow things down much...

--- Advent2018.playground/Pages/Day 16 - Chronal Classification.xcplaygroundpage/Contents.swift
+++ Advent2018.playground/Pages/Day 16 - Chronal Classification.xcplaygroundpage/Contents.swift
@@ -3,8 +3,33 @@
 import Foundation
 import AdventLib
 
-struct Device {
-    var registers: [Int] = [0, 0, 0, 0]
+typealias Operation = (inout Device, Int, Int, Int) -> ()
+
+struct InstructionSet: CustomStringConvertible {
+
+    var opCodes = [[Operation]](repeating: InstructionSet.allMethods, count: 16)
+
+    var description: String {
+        var result = ""
+        for i in 0..<opCodes.count {
+            result.append("code \(i) instructions \(opCodes[i].count)\n")
+        }
+        return result
+    }
+
+    mutating func train(_ testCase: TestCase) {
+        let opCode = testCase.instruction.opCode
+        var instructions = opCodes[opCode]
+        print("testing \(instructions.count) instructions for opcode \(opCode)")
+        instructions = instructions.filter { instruction in
+            var device = Device(registers: testCase.before)
+            device.apply(instruction, a: testCase.instruction.a, b: testCase.instruction.b, c: testCase.instruction.c)
+            print(device.registers, testCase.after)
+            return device.registers == testCase.after
+        }
+        print("result \(instructions.count) instructions")
+        opCodes[testCase.instruction.opCode] = instructions
+    }
 
     // addr (add register) stores into register C the result of adding register A and register B.
     static func addr(device: inout Device, a: Int, b: Int, c: Int) {
@@ -56,32 +81,37 @@ struct Device {
     }
 
     //    Greater-than testing:
-    // gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.
+    // gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise,
+    // register C is set to 0.
     static func gtir(device: inout Device, a: Int, b: Int, c: Int) {
         device.registers[c] = a > device.registers[b] ? 1 : 0
     }
 
-    // gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.
+    // gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise,
+    // register C is set to 0.
     static func gtri(device: inout Device, a: Int, b: Int, c: Int) {
         device.registers[c] = device.registers[a] > b ? 1 : 0
     }
 
-    // gtrr (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
+    // gtrr (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise,
+    // register C is set to 0.
     static func gtrr(device: inout Device, a: Int, b: Int, c: Int) {
         device.registers[c] = device.registers[a] > device.registers[b] ? 1 : 0
     }
 
-//    Equality testing:
-//
-//    eqir (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.
+    // Equality testing:
+    // eqir (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is
+    // set to 0.
     static func eqir(device: inout Device, a: Int, b: Int, c: Int) {
         device.registers[c] = a == device.registers[b] ? 1 : 0
     }
-//    eqri (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.
+    // eqri (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is
+    // set to 0.
     static func eqri(device: inout Device, a: Int, b: Int, c: Int) {
         device.registers[c] = device.registers[a] == b ? 1 : 0
     }
-//    eqrr (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
+    // eqrr (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C
+    // is set to 0.
     static func eqrr(device: inout Device, a: Int, b: Int, c: Int) {
         device.registers[c] = device.registers[a] == device.registers[b] ? 1 : 0
     }
@@ -103,7 +133,34 @@ struct Device {
         eqir,
         eqri,
         eqrr,
-    ]
+        ]
+}
+
+struct Device: CustomStringConvertible {
+    var registers: [Int] = [0, 0, 0, 0]
+
+    init(registers: [Int]) { self.registers = registers }
+
+    mutating func apply(_ operation: Operation, a: Int, b: Int, c: Int) {
+        operation(&self, a, b, c)
+    }
+
+    mutating func execute(instruction: Instruction, instructionSet: InstructionSet) {
+        let opCode = instruction.opCode
+        let operation = instructionSet.opCodes[opCode].first!
+        print(self, instruction)
+        apply(operation, a: instruction.a, b: instruction.b, c: instruction.c)
+    }
+
+    mutating func program(instructions: [Instruction], instructionSet: InstructionSet) {
+        for instruction in instructions {
+            execute(instruction: instruction, instructionSet: instructionSet)
+        }
+    }
+
+    var description: String {
+        return "\(registers)"
+    }
 }
 
 let input = """
@@ -116,7 +173,7 @@ Before: [3, 2, 1, 1]
 After:  [3, 2, 2, 1]
 """
 
-struct Operation {
+struct Instruction {
     var opCode: Int
     var a: Int
     var b: Int
@@ -135,7 +192,7 @@ struct Operation {
 struct TestCase {
     static let regex = try! Regex(pattern: ".*\\[(\\d), (\\d), (\\d), (\\d)\\]")
     var before: [Int]
-    var operation: Operation
+    var instruction: Instruction
     var after: [Int]
 
     init?(input: String) {
@@ -145,8 +202,8 @@ struct TestCase {
             return nil
         }
         self.before = before
-        guard let operation = Operation(line: lines[1]) else { return nil }
-        self.operation = operation
+        guard let instruction = Instruction(line: lines[1]) else { return nil }
+        self.instruction = instruction
         guard let after = TestCase.regex.match(input: lines[2])?.compactMap({ Int($0) }) else {
             return nil
         }
@@ -155,9 +212,9 @@ struct TestCase {
 
     func validOperations() -> Int {
         var valid = 0
-        for method in Device.allMethods {
+        for method in InstructionSet.allMethods {
             var device = Device(registers: before)
-            method(&device, operation.a, operation.b, operation.c)
+            method(&device, instruction.a, instruction.b, instruction.c)
             if device.registers == after {
                 valid += 1
             }
@@ -174,9 +231,26 @@ print(testCases.map { $0.validOperations() })
 
 let url = Bundle.main.url(forResource: "day16.input", withExtension: "txt")!
 let day16input = try! String(contentsOf: url)
-let day16cases = day16input.components(separatedBy: "\n\n").compactMap(TestCase.init)
+let day16sections = day16input.components(separatedBy: "\n\n\n\n")
+let day16cases = day16sections[0].components(separatedBy: "\n\n").compactMap(TestCase.init)
+print(day16cases.count)
 let day16results = day16cases.map { $0.validOperations() }
 print(day16results)
-print(day16results.filter { $0 >= 3}.count)
+print(day16results.filter { $0 >= 3 }.count)
+
+let day16instructions = day16sections[1].components(separatedBy: "\n").compactMap(Instruction.init)
+
+var instructionSet = InstructionSet()
+for i in 0..<16 {
+    let testCases = day16cases.filter { $0.instruction.opCode == i }
+    for testCase in testCases {
+        instructionSet.train(testCase)
+    }
+}
+print(instructionSet)
+
+var finalDevice = Device(registers: [0, 0, 0, 0])
+finalDevice.program(instructions: day16instructions, instructionSet: instructionSet)
+print(finalDevice)
 
 //: [Next](@next)
