bcbfbd837e8f15b2d52fdb5288fd5adb0ae55583
David Paschich <dpassage@balveda.com>
2018-12-22 21:27:11 -0800

    Day 16 part 1

--- Advent2018.playground/Pages/Day 16 - Chronal Classification.xcplaygroundpage/Contents.swift
+++ Advent2018.playground/Pages/Day 16 - Chronal Classification.xcplaygroundpage/Contents.swift
@@ -1,90 +1,182 @@
 //: [Previous](@previous)
 
 import Foundation
+import AdventLib
 
 struct Device {
     var registers: [Int] = [0, 0, 0, 0]
 
     // addr (add register) stores into register C the result of adding register A and register B.
-    mutating func addr(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] + registers[b]
+    static func addr(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] + device.registers[b]
     }
     // addi (add immediate) stores into register C the result of adding register A and value B.
-    mutating func addi(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] + b
+    static func addi(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] + b
     }
 
     // mulr (multiply register) stores into register C the result of multiplying register A and register B.
-    mutating func mulr(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] * registers[b]
+    static func mulr(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] * device.registers[b]
     }
 
     // muli (multiply immediate) stores into register C the result of multiplying register A and value B.
-    mutating func muli(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] * b
+    static func muli(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] * b
     }
 
     // banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
-    mutating func banr(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] & registers[b]
+    static func banr(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] & device.registers[b]
     }
 
     // bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
-    mutating func bani(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] & b
+    static func bani(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] & b
     }
 
     // borr (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.
-    mutating func borr(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] | registers[b]
+    static func borr(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] | device.registers[b]
     }
 
     // bori (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B.
-    mutating func bori(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] | b
+    static func bori(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] | b
     }
 
     // setr (set register) copies the contents of register A into register C. (Input B is ignored.)
-    mutating func setr(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a]
+    static func setr(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a]
     }
 
     // seti (set immediate) stores value A into register C. (Input B is ignored.)
-    mutating func seti(a: Int, b: Int, c: Int) {
-        registers[c] = a
+    static func seti(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = a
     }
 
     //    Greater-than testing:
     // gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.
-    mutating func gtir(a: Int, b: Int, c: Int) {
-        registers[c] = a > registers[b] ? 1 : 0
+    static func gtir(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = a > device.registers[b] ? 1 : 0
     }
 
     // gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.
-    mutating func gtri(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] > b ? 1 : 0
+    static func gtri(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] > b ? 1 : 0
     }
 
     // gtrr (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
-    mutating func gtrr(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] > registers[b] ? 1 : 0
+    static func gtrr(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] > device.registers[b] ? 1 : 0
     }
 
 //    Equality testing:
 //
 //    eqir (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.
-    mutating func eqir(a: Int, b: Int, c: Int) {
-        registers[c] = a == registers[b] ? 1 : 0
+    static func eqir(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = a == device.registers[b] ? 1 : 0
     }
 //    eqri (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.
-    mutating func eqri(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] == b ? 1 : 0
+    static func eqri(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] == b ? 1 : 0
     }
 //    eqrr (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
-    mutating func eqrr(a: Int, b: Int, c: Int) {
-        registers[c] = registers[a] == registers[b] ? 1 : 0
+    static func eqrr(device: inout Device, a: Int, b: Int, c: Int) {
+        device.registers[c] = device.registers[a] == device.registers[b] ? 1 : 0
+    }
+
+    static var allMethods = [
+        addr,
+        addi,
+        mulr,
+        muli,
+        banr,
+        bani,
+        borr,
+        bori,
+        setr,
+        seti,
+        gtir,
+        gtri,
+        gtrr,
+        eqir,
+        eqri,
+        eqrr,
+    ]
+}
+
+let input = """
+Before: [3, 2, 1, 1]
+9 2 1 2
+After:  [3, 2, 2, 1]
+
+Before: [3, 2, 1, 1]
+9 2 1 2
+After:  [3, 2, 2, 1]
+"""
+
+struct Operation {
+    var opCode: Int
+    var a: Int
+    var b: Int
+    var c: Int
+
+    init?(line: String) {
+        let numbers = line.components(separatedBy: " ").compactMap { Int($0) }
+        guard numbers.count == 4 else { return nil }
+        opCode = numbers[0]
+        a = numbers[1]
+        b = numbers[2]
+        c = numbers[3]
     }
 }
 
+struct TestCase {
+    static let regex = try! Regex(pattern: ".*\\[(\\d), (\\d), (\\d), (\\d)\\]")
+    var before: [Int]
+    var operation: Operation
+    var after: [Int]
+
+    init?(input: String) {
+        let lines = input.components(separatedBy: "\n")
+        guard lines.count == 3 else { return nil }
+        guard let before = TestCase.regex.match(input: lines[0])?.compactMap({ Int($0) }) else {
+            return nil
+        }
+        self.before = before
+        guard let operation = Operation(line: lines[1]) else { return nil }
+        self.operation = operation
+        guard let after = TestCase.regex.match(input: lines[2])?.compactMap({ Int($0) }) else {
+            return nil
+        }
+        self.after = after
+    }
+
+    func validOperations() -> Int {
+        var valid = 0
+        for method in Device.allMethods {
+            var device = Device(registers: before)
+            method(&device, operation.a, operation.b, operation.c)
+            if device.registers == after {
+                valid += 1
+            }
+
+        }
+        return valid
+    }
+}
+
+let testCases = input.components(separatedBy: "\n\n").compactMap(TestCase.init)
+print(testCases)
+
+print(testCases.map { $0.validOperations() })
+
+let url = Bundle.main.url(forResource: "day16.input", withExtension: "txt")!
+let day16input = try! String(contentsOf: url)
+let day16cases = day16input.components(separatedBy: "\n\n").compactMap(TestCase.init)
+let day16results = day16cases.map { $0.validOperations() }
+print(day16results)
+print(day16results.filter { $0 >= 3}.count)
 
 //: [Next](@next)
