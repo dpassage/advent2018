b159d72277e3ebed878360396a754aba840c3d94
David Paschich <dpassage@balveda.com>
2018-12-27 22:41:35 -0800

    moar day 24

--- Advent2018.playground/Pages/Day 24 - Immune System Simulator 20XX.xcplaygroundpage/Contents.swift
+++ Advent2018.playground/Pages/Day 24 - Immune System Simulator 20XX.xcplaygroundpage/Contents.swift
@@ -4,11 +4,21 @@ import Foundation
 
 var str = "Hello, playground"
 
+enum AttackType {
+    case fire
+    case slashing
+    case bludgeoning
+    case radiation
+    case cold
+}
 struct Group {
     var units: Int
     var hitPoints: Int
     var attackDamage: Int
+    var attackType: AttackType
     var initiative: Int
-    
+
+    var weak: [AttackType]
+    var immune: [AttackType]
 }
 //: [Next](@next)
